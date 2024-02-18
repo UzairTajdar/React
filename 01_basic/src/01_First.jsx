7bc142d88a29e8830824f74afb1513ee38422325
UzairTajdar <126019289+UzairTajdar@users.noreply.github.com>
2024-02-18 19:52:07 +0500

    First React File

---
+++ 01_basic/src/01_First.jsx
@@ -0,0 +1,7 @@
+function First(){
+    return (
+        <h2>This is my first component</h2>
+    )
+}
+
+export default First // export the component 'First' so that it can be imported in other files
---
+++ 01_basic/src/App.jsx
@@ -0,0 +1,13 @@
+import First from './01_First' // import the component 'First' from the file '01_First.jsx'
+
+function App() {
+  return (
+    <>
+    <h1>First Component</h1>
+   <First />
+   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, a.</p>
+   </>
+  )
+}
+
+export default App
