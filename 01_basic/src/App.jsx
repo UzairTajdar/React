5b12c5f1e14490f816f2542f76a74e070e670a85
UzairTajdar <126019289+UzairTajdar@users.noreply.github.com>
2024-02-19 21:16:00 +0500

    Update App.jsx

--- 01_basic/src/App.jsx
+++ 01_basic/src/App.jsx
@@ -1,9 +1,11 @@
 import First from './01_First' // import the component 'First' from the file '01_First.jsx'
 
 function App() {
+  const username = 'Uzair Tajdar'
+
   return (
     <>
-    <h1>First Component</h1>
+    <h1>First Component {username}</h1>
    <First />
    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, a.</p>
    </>
