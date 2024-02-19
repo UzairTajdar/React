fb00047230cb7ccbbf89c71e4c3946692ac134c5
UzairTajdar <126019289+UzairTajdar@users.noreply.github.com>
2024-02-19 21:16:06 +0500

    Update main.jsx

--- 01_basic/src/main.jsx
+++ 01_basic/src/main.jsx
@@ -2,8 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <>
+    <h1>First Component</h1>
+   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, a.</p>
+   </>
+  )
+}
+
+const Second = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const username = 'Uzair Tajdar'
+const reactElement = React.createElement(
+  'a',
+   { href: 'https://google.com', target: '_blank' },
+    'Visit Google ',
+    username
+    )
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  reactElement
 )
