df848bb96eb054943494327cb5ddd4277cb39eb4
UzairTajdar <126019289+UzairTajdar@users.noreply.github.com>
2024-02-22 23:07:37 +0500

    password generaor

---
+++ 05_passwordGenerator/src/App.jsx
@@ -0,0 +1,88 @@
+import { useState,useCallback,useEffect,useRef } from 'react'
+
+
+function App() {
+  const [length, setlength] = useState(8)
+  const [numAllowed, setNumAllowed] = useState(false)
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState('') 
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
+    let pass = '';
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+
+    if (numAllowed) str += '0123456789';
+    if (charAllowed) str += '!@#$%^&*_+';
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char)
+    }
+    setPassword(pass)
+  } ,[numAllowed,charAllowed,length,setPassword])
+
+  const SavePasswordInClipboard = useCallback(()=>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[length,numAllowed,charAllowed,passwordGenerator])
+
+  return (
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+      <h1 className='text-white text-center my-3 '>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type="text"
+           placeholder='Password'
+            value={password}
+            readOnly
+            className='outline-none w-full py-1 px-3'
+            ref={passwordRef}
+            
+        />
+        <button
+         onClick={SavePasswordInClipboard}
+         className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>
+         Copy
+        </button>
+      </div>
+      <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+              <input type="range"
+                     min={8}
+                     max={100}
+                     value={length}
+                     className='cursor-pointer'
+                     onChange={(e) => setlength(e.target.value)}
+              />
+              <label>{length}</label>
+          </div>
+          <div className='flex items-center gap-x-1'>
+            <input type="checkbox"
+                  defaultChecked = {numAllowed}
+                  id='numberInput'
+                  onChange={() =>{
+                    setNumAllowed((prev) => !prev);}} />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+          <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                  setCharAllowed((prev) => !prev )
+              }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+      </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
